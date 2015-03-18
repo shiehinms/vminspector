d50d9aaf89457a785977880fb0c48e281c6b8ed8
shiehinms <t-zhixie@microsoft.com>
2015-03-18 14:05:58 +0800

    Ready to retrieve data from inode.

--- formats.py
+++ formats.py
@@ -15,7 +15,7 @@ BLong = SBInt32
 UBInt32 = UBInt32
 
 
-superblock = Struct("superblock",
+Superblock = Struct("superblock",
                 ULInt32('inodes_count'),
                 ULInt32('blocks_count'),
                 ULInt32('reserved_blocks_count'),
@@ -43,29 +43,193 @@ superblock = Struct("superblock",
                 ULInt32('checkinterval'),
                 ULInt32('creator_os'),
                 ULInt32('rev_level'),
-                Padding(235 * 4),
+                ULInt16('def_resuid'),
+                ULInt16('def_resgid'),
+                ULInt32('first_ino'),
+                ULInt16('inode_size'),
+                ULInt16('block_group_nr'),
+                ULInt32('feature_compat'),
+                Padding(464),
                 )
 
 
-mbr_pe = Struct('mbr_partition_entry',
-                ULInt8('Boot_Indicator'),
+Group_desc = Struct('group_desc',
+                    ULInt32('block_bitmap_ptr'),
+                    ULInt32('inode_bitmap_ptr'),
+                    ULInt32('inode_table_ptr'),
+                    ULInt16('free_blocks_count'),
+                    ULInt16('free_inodes_count'),
+                    ULInt16('used_dirs_count'),
+                    ULInt16('pad'),
+                    Array(3, ULInt32('reversed')),
+                    )
+
+
+Group_desc_table0 = Struct('group_desc_table0',
+                           Array(32, Group_desc),
+                          )
+
+
+Group_desc_table1 = Struct('group_desc_table1',
+                           Array(64, Group_desc),
+                          )
+
+
+Group_desc_table2 = Struct('group_desc_table2',
+                           Array(128, Group_desc),
+                          )
+
+
+Inode_128 = Struct("inode",
+               FlagsEnum(ULInt16('mode'),
+                         IFSOCK =  0xC000,
+                         IFLNK  =  0xA000,
+                         IFREG  =  0x8000,
+                         IFBLK  =  0x6000,
+                         IFDIR  =  0x4000,
+                         IFCHR  =  0x2000,
+                         IFIFO  =  0x1000,
+                         ISUID  =  0x0800,
+                         ISGID  =  0x0400,
+                         ISVTX  =  0x0200,
+                         IRUSR  =  0x0100,
+                         IWUSR  =  0x0080,
+                         IXUSR  =  0x0040,
+                         IRGRP  =  0x0020,
+                         IWGRP  =  0x0010,
+                         IXGRP  =  0x0008,
+                         IROTH  =  0x0004,
+                         IWOTH  =  0x0002,
+                         IXOTH  =  0x0001,
+                         ),
+               ULInt16('uid'),
+               ULInt32('size'),
+               ULInt32('atime'),
+               ULInt32('ctime'),
+               ULInt32('mtime'),
+               ULInt32('dtime'),
+               ULInt16('gid'),
+               ULInt16('links_count'),
+               ULInt32('blocks'),
+               FlagsEnum(ULInt32('flags'),
+                        SECRM        = 0x00000001,
+                        UNRM         = 0x00000002,
+                        COMPR        = 0x00000004,
+                        SYNC         = 0x00000008,
+                        IMMUTABLE    = 0x00000010,
+                        APPEND       = 0x00000020,
+                        NODUMP       = 0x00000040,
+                        NOATIME      = 0x00000080,
+                        DIRTY        = 0x00000100,
+                        COMPRBLK     = 0x00000200,
+                        NOCOMPR      = 0x00000400,
+                        ECOMPR       = 0x00000800,
+                        BTREE        = 0x00001000,
+                        INDEX        = 0x00001000,
+                        IMAGIC       = 0x00002000,
+                        JOURNAL_DATA = 0x00004000,
+                        RESERVED     = 0x80000000,
+                        ),
+               ULInt32('i_reserved1'),
+               Array(15, ULInt32('blocks_ptr')),
+               Padding(28),
+               )
+
+
+Inode_256 = Struct("inode",
+               FlagsEnum(ULInt16('mode'),
+                         IFSOCK =  0xC000,
+                         IFLNK  =  0xA000,
+                         IFREG  =  0x8000,
+                         IFBLK  =  0x6000,
+                         IFDIR  =  0x4000,
+                         IFCHR  =  0x2000,
+                         IFIFO  =  0x1000,
+                         ISUID  =  0x0800,
+                         ISGID  =  0x0400,
+                         ISVTX  =  0x0200,
+                         IRUSR  =  0x0100,
+                         IWUSR  =  0x0080,
+                         IXUSR  =  0x0040,
+                         IRGRP  =  0x0020,
+                         IWGRP  =  0x0010,
+                         IXGRP  =  0x0008,
+                         IROTH  =  0x0004,
+                         IWOTH  =  0x0002,
+                         IXOTH  =  0x0001,
+                         ),
+               ULInt16('uid'),
+               ULInt32('size'),
+               ULInt32('atime'),
+               ULInt32('ctime'),
+               ULInt32('mtime'),
+               ULInt32('dtime'),
+               ULInt16('gid'),
+               ULInt16('links_count'),
+               ULInt32('blocks'),
+               FlagsEnum(ULInt32('flags'),
+                        SECRM        = 0x00000001,
+                        UNRM         = 0x00000002,
+                        COMPR        = 0x00000004,
+                        SYNC         = 0x00000008,
+                        IMMUTABLE    = 0x00000010,
+                        APPEND       = 0x00000020,
+                        NODUMP       = 0x00000040,
+                        NOATIME      = 0x00000080,
+                        DIRTY        = 0x00000100,
+                        COMPRBLK     = 0x00000200,
+                        NOCOMPR      = 0x00000400,
+                        ECOMPR       = 0x00000800,
+                        BTREE        = 0x00001000,
+                        INDEX        = 0x00001000,
+                        IMAGIC       = 0x00002000,
+                        JOURNAL_DATA = 0x00004000,
+                        RESERVED     = 0x80000000,
+                        ),
+               ULInt32('i_reserved1'),
+               Array(15, ULInt32('blocks_ptr')),
+               Padding(156),
+               )
+
+
+# special inodes
+EXT2_BAD_INO = 1
+EXT2_ROOT_INO = 2
+EXT2_ACL_IDX_INO = 3
+EXT2_ACL_DATA_INO = 4
+EXT2_BOOT_LOADER_INO = 5
+EXT2_UNDEL_DIR_INO = 6
+EXT2_FIRST_INO = 11 
+
+
+Dir_entry = Struct("dir_entry",
+                   ULInt32("inode"),
+                   ULInt16("rec_length"),
+                   ULInt8("name_length"),
+                   ULInt8("file_type"),
+                   Field("name", lambda ctx: ctx["name_length"]),
+                   )
+
+
+Mbr_pe = Struct('mbr_partition_entry',
+                ULInt8('boot_indicator'),
                 Padding(3),
-                ULInt8('Partition_Type'),
+                ULInt8('partition_type'),
                 Padding(3),
-                ULInt32('Starting_Sector'),
-                ULInt32('Total_Sector'),
+                ULInt32('starting_sector'),
+                ULInt32('total_sector'),
                 )
 
 
-mbr = Struct('MBR',
-             Array(446, SBInt8('Bootstrapper')),
-             Array(4, mbr_pe),
+Mbr = Struct('mbr',
+             Array(446, SBInt8('bootstrapper')),
+             Array(4, Mbr_pe),
              ULInt8('signature1'),
              ULInt8('signature2'),
              )
 
 
-hd_ftr = Struct("hd_ftr",
+Hd_ftr = Struct("hd_ftr",
                 Array(8, SBInt8('cookie')),
                 UBInt32('feature'),
                 UBInt32('ff_version'),
--- inspector.py
+++ inspector.py
@@ -11,6 +11,8 @@ from formats import *
 
 VERSION = 'v1.0.0'
 USAGE = 'usage: %prog [options] arg1 arg2'
+HD_TYPE_FIXED = 2
+HD_TYPE_DYNAMIC = 3
 
 
 def get_options():
@@ -23,24 +25,175 @@ def get_options():
                       help='Account Name', dest='account_name', default='')
     parser.add_option('-k', '--key', action='store', type='string',
                       help='Account Key', dest='account_key', default='')
+    parser.add_option('-v', '--vhd', action='store', type='string',
+                      help='VHD File', dest='vhd', default='')
+    parser.add_option('-c', '--container', action='store', type='string',
+                      help='Container Name', dest='container', default='vhds')
     parser.add_option('-b', '--hostbase', action='store', type='string',
                       help='Host Base', dest='host_base', default='.blob.core.windows.net')
     parser.add_option('-t', '--test', action='store_true', dest='test',
                       help='Test the difference between sync and async.')
-    parser.add_option('-v', '--verbose', action='store_true', dest='verbose',
-                      help='Output verbosely.')
 
     return parser.parse_args()
 
 
-def get_data(blob_service, container_name, blob_name, from_to):
-    """TODO: Docstring for get_data.
+def check_vhd_type(blob_service, options):
+    """TODO: Docstring for check_vhd_type.
 
     :blob_service: TODO
+    :properties: TODO
+    :options: TODO
     :returns: TODO
 
     """
-    return blob_service.get_blob(container_name, blob_name, x_ms_range=from_to)
+    properties = blob_service.get_blob_properties(options.container, options.vhd)
+    rangerange = 'bytes=' + str(int(properties['content-length']) - 512) + \
+            '-' + str(int(properties['content-length'])-1)
+    blob_page = blob_service.get_blob(options.container,
+                                    options.vhd,
+                                    x_ms_range=rangerange)
+    hd_ftr = Hd_ftr.parse(blob_page)
+
+    return hd_ftr.type
+
+
+def get_superblock(blob_service, start_at_byte, options):
+    """TODO: Docstring for get_superblock.
+
+    :blob_service: TODO
+    :partition: TODO
+    :options: TODO
+    :returns: TODO
+
+    """
+    rangerange = 'bytes=' + str(start_at_byte+1024) + \
+            '-' + str(start_at_byte+1024+1023)
+    blob_page = blob_service.get_blob(options.container,
+                                    options.vhd,
+                                    x_ms_range=rangerange)
+
+    return Superblock.parse(blob_page)
+
+
+def get_group_desc_table(blob_service, start_at_byte, options):
+    """TODO: Docstring for get_group_desc_table.
+
+    :blob_service: TODO
+    :start_at_byte: TODO
+    :options: TODO
+    :returns: TODO
+
+    """
+    # Can be beautified. Pendding.
+    if options.block_size == 1024:
+        offset = 2048
+        Group_desc_table = Group_desc_table0
+    if options.block_size == 2048:
+        offset = 2048
+        Group_desc_table = Group_desc_table1
+    if options.block_size == 4096:
+        offset = 4096
+        Group_desc_table = Group_desc_table2
+
+    rangerange = 'bytes=' + str(start_at_byte+offset) + \
+            '-' + str(start_at_byte+offset+options.block_size-1)
+    blob_page = blob_service.get_blob(options.container,
+                                    options.vhd,
+                                    x_ms_range=rangerange)
+
+    return Group_desc_table.parse(blob_page)
+
+
+def retrieve_inode_file(arg1):
+    """TODO: Docstring for retrieve_inode_file.
+
+    :arg1: TODO
+    :returns: TODO
+
+    """
+    pass
+
+
+def parse_KB(superblock):
+    """TODO: Docstring for parse_KB.
+
+    :superblock: TODO
+    :returns: TODO
+
+    """
+    if superblock.log_block_size == 'OneKB':
+        result = 1024
+    if superblock.log_block_size == 'TwoKB':
+        result = 2048
+    if superblock.log_block_size == 'FourKB':
+        result = 4096
+
+    return result
+
+
+def parse_partition(blob_service, partition, options):
+    """TODO: Docstring for parse_partition.
+
+    :blob_service: TODO
+    :partition: TODO
+    :options: TODO
+    :returns: TODO
+
+    """
+    start_at_byte = partition.starting_sector * 512
+    superblock = get_superblock(blob_service, start_at_byte, options)
+    options.block_size = parse_KB(superblock)
+    group_desc_table = get_group_desc_table(blob_service,
+                                              start_at_byte,
+                                              options)
+
+    group_num = ((superblock.blocks_count - superblock.first_data_block - 1) /
+                 superblock.blocks_per_group) + 1
+
+    for group_desc in group_desc_table.group_desc:
+        if superblock.inode_size == 128:
+            Inode = Inode_128
+        if superblock.inode_size == 256:
+            Inode = Inode_256
+        offset = options.block_size * group_desc.inode_table_ptr
+        table_size = superblock.inodes_per_group * superblock.inode_size
+        Inode_table = Struct('inode_table',
+                             Array(superblock.inodes_per_group, Inode),
+                             )
+        rangerange = 'bytes=' + str(start_at_byte+offset) + \
+                '-' + str(start_at_byte+offset+table_size-1)
+        blob_page = blob_service.get_blob(options.container,
+                                        options.vhd,
+                                        x_ms_range=rangerange)
+        inode_table = Inode_table.parse(blob_page)
+        print inode_table.inode[1]
+        print inode_table.inode[2006]
+        print inode_table.inode[670]
+        print group_desc
+        exit(0)
+
+    print partition
+    print superblock
+    print group_num
+
+
+def parse_image(blob_service, options):
+    """TODO: Docstring for parse_image.
+
+    :blob_service: TODO
+    :options: TODO
+    :returns: TODO
+
+    """
+    blob_page = blob_service.get_blob(options.container,
+                                    options.vhd,
+                                    x_ms_range='bytes=0-511')
+
+    mbr = Mbr.parse(blob_page)
+
+    for partition in mbr.mbr_partition_entry:
+        partition.boot_indicator == 0x80 and \
+                parse_partition(blob_service, partition, options)
 
 
 def main():
@@ -53,28 +206,9 @@ def main():
     blob_service = BlobService(options.account_name,
                                options.account_key,
                                host_base=options.host_base)
-    containers = blob_service.list_containers()
 
-    for container in containers:
-        print container.name
-        for blob in blob_service.list_blobs(container.name):
-            print blob.name
-
-        result = blob_service.get_page_ranges(container.name, 'shieh-shieh-2015-03-15.vhd')
-        print result[0].start, result[0].end
-
-        properties =  blob_service.get_blob_properties(container.name, 'shieh-shieh-2015-03-15.vhd')
-        rangerange = 'bytes=' + str(int(properties['content-length']) - 512) + '-' + str(int(properties['content-length'])-1)
-        print rangerange
-        result1 = blob_service.get_blob(container.name, 'shieh-shieh-2015-03-15.vhd', x_ms_range=rangerange)
-        print hd_ftr.parse(result1)
-
-        result2 = blob_service.get_blob(container.name, 'shieh-shieh-2015-03-15.vhd', x_ms_range='bytes=0-511')
-        with open('result.txt', 'w') as output:
-            output.write(result2)
-        print mbr.parse(result2)
-        # result2 = blob_service.get_blob(container.name, 'shieh-shieh-2015-03-15.vhd', x_ms_range='bytes=1536-2559')
-        # print superblock.parse(result2)
+    check_vhd_type(blob_service, options) == HD_TYPE_FIXED and \
+            parse_image(blob_service, options)
 
 
 if __name__ == '__main__':
