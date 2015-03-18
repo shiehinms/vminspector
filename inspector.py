849920bc3cc59b99f5656ddd097c93bdf57c9630
shiehinms <t-zhixie@microsoft.com>
2015-03-18 17:19:20 +0800

    i-block read err. pedding

--- inspector.py
+++ inspector.py
@@ -37,15 +37,15 @@ def get_options():
     return parser.parse_args()
 
 
-def check_vhd_type(blob_service, options):
+def check_vhd_type(options):
     """TODO: Docstring for check_vhd_type.
 
-    :blob_service: TODO
     :properties: TODO
     :options: TODO
     :returns: TODO
 
     """
+    blob_service = options.blob_service
     properties = blob_service.get_blob_properties(options.container, options.vhd)
     rangerange = 'bytes=' + str(int(properties['content-length']) - 512) + \
             '-' + str(int(properties['content-length'])-1)
@@ -57,28 +57,22 @@ def check_vhd_type(blob_service, options):
     return hd_ftr.type
 
 
-def get_superblock(blob_service, start_at_byte, options):
+def get_superblock(start_at_byte, options):
     """TODO: Docstring for get_superblock.
 
-    :blob_service: TODO
     :partition: TODO
     :options: TODO
     :returns: TODO
 
     """
-    rangerange = 'bytes=' + str(start_at_byte+1024) + \
-            '-' + str(start_at_byte+1024+1023)
-    blob_page = blob_service.get_blob(options.container,
-                                    options.vhd,
-                                    x_ms_range=rangerange)
+    blob_page = get_blob_page(start_at_byte, 1024, 1024, options)
 
     return Superblock.parse(blob_page)
 
 
-def get_group_desc_table(blob_service, start_at_byte, options):
+def get_group_desc_table(start_at_byte, options):
     """TODO: Docstring for get_group_desc_table.
 
-    :blob_service: TODO
     :start_at_byte: TODO
     :options: TODO
     :returns: TODO
@@ -95,23 +89,153 @@ def get_group_desc_table(blob_service, start_at_byte, options):
         offset = 4096
         Group_desc_table = Group_desc_table2
 
-    rangerange = 'bytes=' + str(start_at_byte+offset) + \
-            '-' + str(start_at_byte+offset+options.block_size-1)
-    blob_page = blob_service.get_blob(options.container,
-                                    options.vhd,
-                                    x_ms_range=rangerange)
+    blob_page = get_blob_page(start_at_byte, offset, options.block_size, options)
 
     return Group_desc_table.parse(blob_page)
 
 
-def retrieve_inode_file(arg1):
-    """TODO: Docstring for retrieve_inode_file.
+def get_blob_page(start_at_byte, offset, page_size, options):
+    """TODO: Docstring for get_blob_page.
 
-    :arg1: TODO
+    :offset: TODO
+    :page_size: TODO
+    :options: TODO
     :returns: TODO
 
     """
-    pass
+    rangerange = 'bytes=' + str(start_at_byte+offset) + \
+            '-' + str(start_at_byte+offset+page_size-1)
+    blob_page = options.blob_service.get_blob(options.container,
+                                              options.vhd,
+                                              x_ms_range=rangerange)
+
+    return blob_page
+
+
+def get_data_from_dir_ptr(dir_ptr, start_at_byte, options):
+    """TODO: Docstring for get_data_from_dir_ptr.
+
+    :dir_ptr: TODO
+    :start_at_byte: TODO
+    :options: TODO
+    :returns: TODO
+
+    """
+    offset = block_ptr_to_byte(dir_ptr, options)
+    blob_page = get_blob_page(start_at_byte, offset,
+                              options.block_size, options)
+
+    return blob_page
+
+
+def get_data_from_indir_ptr1(indir_ptr, start_at_byte, options):
+    """TODO: Docstring for get_data_from_indir_ptr1.
+
+    :indir_ptr: TODO
+    :start_at_byte: TODO
+    :options: TODO
+    :returns: TODO
+
+    """
+    Indir_ptr_list = Struct('indir_ptr_list',
+                            Array(options.block_size/4, ULInt32('indir_ptr')),
+                            )
+    offset = block_ptr_to_byte(dir_ptr, options)
+    blob_page = get_blob_page(start_at_byte, offset, options.block_size, options)
+    indir_ptr_list = Indir_ptr_list.parse(blob_page)
+
+    data = ''
+    for dir_ptr in indir_ptr_list.indir_ptr_list:
+        data = data + get_data_from_dir_ptr(dir_ptr, start_at_byte, options)
+
+    return data
+
+
+def get_data_from_indir_ptr2(indir_ptr, start_at_byte, options):
+    """TODO: Docstring for get_data_from_indir_ptr2.
+
+    :indir_ptr: TODO
+    :start_at_byte: TODO
+    :options: TODO
+    :returns: TODO
+
+    """
+    Indir_ptr_list = Struct('indir_ptr_list',
+                            Array(options.block_size/4, ULInt32('indir_ptr')),
+                            )
+    offset = block_ptr_to_byte(dir_ptr, options)
+    blob_page = get_blob_page(start_at_byte, offset, options.block_size, options)
+    indir_ptr_list = Indir_ptr_list.parse(blob_page)
+
+    data = ''
+    for indir_ptr1 in indir_ptr_list.indir_ptr_list:
+        data = data + get_data_from_indir_ptr1(dir_ptr, start_at_byte, options)
+
+    return data
+
+
+def get_data_from_indir_ptr3(indir_ptr, start_at_byte, options):
+    """TODO: Docstring for get_data_from_indir_ptr3.
+
+    :indir_ptr: TODO
+    :start_at_byte: TODO
+    :options: TODO
+    :returns: TODO
+
+    """
+    Indir_ptr_list = Struct('indir_ptr_list',
+                            Array(options.block_size/4, ULInt32('indir_ptr')),
+                            )
+    offset = block_ptr_to_byte(dir_ptr, options)
+    blob_page = get_blob_page(start_at_byte, offset, options.block_size, options)
+    indir_ptr_list = Indir_ptr_list.parse(blob_page)
+
+    data = ''
+    for indir_ptr2 in indir_ptr_list.indir_ptr_list:
+        data = data + get_data_from_indir_ptr2(dir_ptr, start_at_byte, options)
+
+    return data
+
+
+def get_data_from_inode(inode, start_at_byte, options):
+    """TODO: Docstring for get_data_from_inode.
+
+    :inode: TODO
+    :start_at_byte: TODO
+    :options: TODO
+    :returns: TODO
+
+    """
+    data = ''
+    for index, ptr in enumerate(inode.blocks_ptr):
+        if ptr == 0:
+            break
+
+        offset = block_ptr_to_byte(ptr, options)
+        blob_page = get_blob_page(start_at_byte, offset,
+                                  options.block_size, options)
+
+        if index < 12:
+            data = data + blob_page
+        elif index == 12:
+            data = get_data_from_indir_ptr1(ptr, start_at_byte, options)
+        elif index == 13:
+            data = get_data_from_indir_ptr2(ptr, start_at_byte, options)
+        elif index == 14:
+            data = get_data_from_indir_ptr3(ptr, start_at_byte, options)
+
+    return data
+
+
+def block_ptr_to_byte(block_ptr, options):
+    """TODO: Docstring for block_ptr_to_byte.
+
+    :block_ptr: TODO
+    :options: TODO
+    :returns: TODO
+
+    """
+    return options.block_size * block_ptr
 
 
 def parse_KB(superblock):
@@ -131,21 +255,18 @@ def parse_KB(superblock):
     return result
 
 
-def parse_partition(blob_service, partition, options):
+def parse_partition(partition, options):
     """TODO: Docstring for parse_partition.
 
-    :blob_service: TODO
     :partition: TODO
     :options: TODO
     :returns: TODO
 
     """
     start_at_byte = partition.starting_sector * 512
-    superblock = get_superblock(blob_service, start_at_byte, options)
+    superblock = get_superblock(start_at_byte, options)
     options.block_size = parse_KB(superblock)
-    group_desc_table = get_group_desc_table(blob_service,
-                                              start_at_byte,
-                                              options)
+    group_desc_table = get_group_desc_table(start_at_byte, options)
 
     group_num = ((superblock.blocks_count - superblock.first_data_block - 1) /
                  superblock.blocks_per_group) + 1
@@ -155,37 +276,35 @@ def parse_partition(blob_service, partition, options):
             Inode = Inode_128
         if superblock.inode_size == 256:
             Inode = Inode_256
-        offset = options.block_size * group_desc.inode_table_ptr
-        table_size = superblock.inodes_per_group * superblock.inode_size
         Inode_table = Struct('inode_table',
                              Array(superblock.inodes_per_group, Inode),
                              )
-        rangerange = 'bytes=' + str(start_at_byte+offset) + \
-                '-' + str(start_at_byte+offset+table_size-1)
-        blob_page = blob_service.get_blob(options.container,
-                                        options.vhd,
-                                        x_ms_range=rangerange)
+        offset = block_ptr_to_byte(group_desc.inode_table_ptr, options)
+        table_size = superblock.inodes_per_group * superblock.inode_size
+        blob_page = get_blob_page(start_at_byte, offset, table_size, options)
         inode_table = Inode_table.parse(blob_page)
+
+        print inode_table.inode[173]
+        print get_data_from_inode(inode_table.inode[173], start_at_byte, options)
+        exit(0)
         print inode_table.inode[1]
+        print inode_table.inode[10]
         print inode_table.inode[2006]
         print inode_table.inode[670]
+        print inode_table.inode[171]
+        print inode_table.inode[181]
         print group_desc
         exit(0)
 
-    print partition
-    print superblock
-    print group_num
 
-
-def parse_image(blob_service, options):
+def parse_image(options):
     """TODO: Docstring for parse_image.
 
-    :blob_service: TODO
     :options: TODO
     :returns: TODO
 
     """
-    blob_page = blob_service.get_blob(options.container,
+    blob_page = options.blob_service.get_blob(options.container,
                                     options.vhd,
                                     x_ms_range='bytes=0-511')
 
@@ -193,7 +312,7 @@ def parse_image(blob_service, options):
 
     for partition in mbr.mbr_partition_entry:
         partition.boot_indicator == 0x80 and \
-                parse_partition(blob_service, partition, options)
+                parse_partition(partition, options)
 
 
 def main():
@@ -206,9 +325,10 @@ def main():
     blob_service = BlobService(options.account_name,
                                options.account_key,
                                host_base=options.host_base)
+    options.blob_service = blob_service
 
-    check_vhd_type(blob_service, options) == HD_TYPE_FIXED and \
-            parse_image(blob_service, options)
+    check_vhd_type(options) == HD_TYPE_FIXED and \
+            parse_image(options)
 
 
 if __name__ == '__main__':
